4ca0ff298dba5c67258bd6ff2193fc0b18269b1a
gastonmardones <gastonmardones22@gmail.com>
2021-05-19 22:46:59 -0300

    Configuro BBDD mysql en settings
    Install mysql
    migrate

--- EduAnalytica/settings.py
+++ EduAnalytica/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'EduAnalytica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eduanalytica',
+        'USER': 'root',
+        'PASSWORD': 'gaston22',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
