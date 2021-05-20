ddf5e7cc10f35a6d7244720222d029b21426495c
gastonmardones <gastonmardones22@gmail.com>
2021-05-20 13:33:20 -0300

    Creo la vista y el path url

--- app_cuenta/views.py
+++ app_cuenta/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from app_cuenta.models import Transaccion
 
 # Create your views here.
+
+def accounts(request):
+    return render(request, 'app_cuenta/index.html')
+
+
