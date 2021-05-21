f394c5f8d7d67eb6d3c4a8c62063b8b9c498ba89
gastonmardones <gastonmardones22@gmail.com>
2021-05-20 21:18:53 -0300

    Registro models en admin para poder agregar registros

--- app_cuenta/admin.py
+++ app_cuenta/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Transaccion
 
 # Register your models here.
+
+
+class Transaccion_Admin(admin.ModelAdmin):
+    list_display = ('numero_cuenta', 'numero_comprobante', 'tipo_comprobante')
+
+
+admin.site.register(Transaccion, Transaccion_Admin)
