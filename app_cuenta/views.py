2c7eb5fbcf17f02dd051962ae81a958660578e3d
gastonmardones <gastonmardones22@gmail.com>
2021-05-20 21:45:02 -0300

    Guarda los registros del form en la bbdd

--- app_cuenta/views.py
+++ app_cuenta/views.py
@@ -5,7 +5,13 @@ from app_cuenta.models import Transaccion
 # Create your views here.
 
 def transaccion_new(request):
+    if request.method == 'POST':
         form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
+
     return render(request, 'forms/forms.html', {'form': form})
 
 def accounts(request):
