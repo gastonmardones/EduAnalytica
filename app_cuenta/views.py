21e9acd1dacec4de63349f37c1ad4e89de152ddc
gastonmardones <gastonmardones22@gmail.com>
2021-05-20 21:37:05 -0300

    Creo view transaccion_new

--- app_cuenta/views.py
+++ app_cuenta/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from app_cuenta.forms import Form
 from app_cuenta.models import Transaccion
 
 # Create your views here.
+
+def transaccion_new(request):
+        form = Form(request.POST)
+    return render(request, 'forms/forms.html', {'form': form})
 
 def accounts(request):
     transacciones = Transaccion.objects.all()
