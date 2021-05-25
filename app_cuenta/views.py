569e6e4b91c40e7038cbe0e6bf947ab3bc2269d0
gastonmardones <gastonmardones22@gmail.com>
2021-05-24 22:23:34 -0300

    Convierto transaccion_new a Class view

--- app_cuenta/views.py
+++ app_cuenta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from app_cuenta.forms import Form
+from django.urls import reverse_lazy
+from app_cuenta.forms import TransaccionForm
 from app_cuenta.models import Transaccion
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView
+
 
 # Create your views here.
 
@@ -13,10 +15,15 @@ class Blank(TemplateView):
 class Accounts(ListView):
     template_name = 'app_cuenta/index.html'
     model = Transaccion
-    #queryset = Transaccion.objects.all()      # Metodo predefinido dentro de ListView
-    #context_object_name = 'transacciones'     # Sino usa como context: 'nombredelmodelo_list'
+    # queryset = Transaccion.objects.all()      # Metodo predefinido dentro de ListView
+    # context_object_name = 'transacciones'     # Sino usa como context: 'nombredelmodelo_list'
 
 
+class TransaccionCreate(CreateView):
+    model = Transaccion
+    template_name = 'forms/forms.html'
+    form_class = TransaccionForm
+    success_url = reverse_lazy('accounts')
 
 def transaccion_new(request):
     if request.method == 'POST':
